3c2885ae4a78edc8df63e9962ca3cb4decde882a
crazywonton <ktarcza1@binghamton.edu>
2018-10-08 11:23:34 -0400

    Implemented getDistanceToWall() and copmuteControlSignal()

--- assign3.py
+++ assign3.py
@@ -17,26 +17,80 @@ import roslib
 from geometry_msgs.msg import Twist
 
 global laserData
+global distance
+global currentVel
 
 def update_laserScanData(data):
 	laserData = data
 
-def getDistanceToWall(laserScanData):
+def getDistanceToWall(ld):
 	#param the laser scan data
 	#finds the distance to the nearest object if applicable and returns it
-	return 0
+	#640 scans are produced in this case, so we take the middle portion
+	#	which is basically directly in front of the robot
+	i = 315
+	total = 0
+	numEntries = 0
+	while i < 325:
+		if ld[i] > 0.45 && ld[i] < 10.0:
+			total += ld[i]
+			numEntries++
+
+	return (total/numEntries)
 	
-def computeControlSignal(distanceToWall):
+def computeControlSignal(d):
 	#takes in the distance to the wall
 	#returns a control signal
 	def noObstacles():
-		return 0
+		vel_msg = Twist()
+		vel_msg.linear.y = 0
+		vel_msg.linear.z = 0
+		
+		if currentVel < 1:
+			vel_msg.linear.x = currentVel + 0.1
+		else
+			vel_msg.linear.x = currentVel
+		
+		currentVel = vel_msg.linear.x
+		vel_msg.angular.x = 0
+		vel_msg.angular.y = 0
+		vel_msg.angular.z = 0
+		return vel_msg
+		
 	def approachingObstacle():
-		return 0
-	def atObstacle():
-		return 0
+		vel_msg = Twist()
+		vel_msg.linear.y = 0
+		vel_msg.linear.z = 0
+		
+		if currentVel > 0.5:
+			vel_msg.linear.x = currentVel - 0.1
+		else
+			vel_msg.linear.x = currentVel
+		
+		currentVel = vel_msg.linear.x
+		vel_msg.angular.x
+		vel_msg.angular.y = 0
+		vel_msg.angular.z = 0
+		return vel_msg
 
-	return 0
+	def atObstacle():
+		vel_msg = Twist()
+		vel_msg.linear.x = 0
+		currentVel = vel_msg.linear.x
+		vel_msg.linear.y = 0
+		vel_msg.linear.z = 0
+		vel_msg.angular.x = 0
+		vel_msg.angular.y = 0
+		vel_msg.angular.z = 0.5
+		return vel_msg
+
+
+	if d > 3:
+		return noObstacles()
+	elif d > 1:
+		return approachingObstacle()
+	else
+		return atObstacle()
 	
 
 
@@ -56,5 +110,4 @@ if __name__ == '__main__':
 		rospy.sleep(0.1)
 
 		robot_control_signal = computeControlSignal(getDistanceToWall(laserData))
-
 		pubControlSignals.publish(robot_control_signal)
