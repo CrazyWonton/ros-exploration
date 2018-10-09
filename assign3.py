3a86ed26ae3a6b9f957e33de61b114d86e116f44
crazywonton <ktarcza1@binghamton.edu>
2018-10-09 09:18:13 -0400

    Fine tuned the navigation algorithm to explore more efficiently

--- assign3.py
+++ assign3.py
@@ -20,7 +20,8 @@ import math
 
 #left middle right
 distance = [0.0,0.0,0.0]
-state = 'stationary'
+previousDistance = [0.0,0.0,0.0]
+state = ['blocked','blocked']
 
 def update_laserScanData(data):
 	#param the laser scan data
@@ -28,10 +29,9 @@ def update_laserScanData(data):
 	#640 scans are produced in this case, so we take the middle portion
 	#	which is basically directly in front of the robot
 	global distance
-	global state
 	
 	numberOfEntries = len(data.ranges)
-	dataRange = numberOfEntries/50
+	dataRange = numberOfEntries/100
 	lowerBound = (numberOfEntries/2) - dataRange
 	upperBound = (numberOfEntries/2) + dataRange
 	
@@ -39,7 +39,7 @@ def update_laserScanData(data):
 	total = 0
 	numEntries = 0
 	i = 0
-	while i < dataRange:
+	while i < (lowerBound - dataRange):
 		if data.ranges[i] > 0.45 and data.ranges[i] < 10.0:
 			total += data.ranges[i]
 			numEntries+=1
@@ -47,7 +47,7 @@ def update_laserScanData(data):
 	if numEntries > 0:
 		distance[0] = (total/numEntries)
 	else:
-		distance[0] = 0
+		distance[0] = 5
 	
 	#compute distance in center
 	total = 0
@@ -62,26 +62,28 @@ def update_laserScanData(data):
 	if numEntries > 0:
 		distance[1] = (total/numEntries)
 	else:
-		distance[1] = 10
+		distance[1] = 5
 
 	#compute distance on right
 	total = 0
 	numEntries = 0
-	i = numberOfEntries - 1
-	while i > (numberOfEntries-dataRange):
+	i = upperBound
+	while i > (upperBound + dataRange):
 		if data.ranges[i] > 0.45 and data.ranges[i] < 10.0:
 			total += data.ranges[i]
 			numEntries+=1
-		i-=1
+		i+=1
 	if numEntries > 0:
 		distance[2] = (total/numEntries)
 	else:
-		distance[2] = 0
+		distance[2] = 5
 
 def computeControlSignal():
 	#takes in the distance to the wall
 	#returns a control signal
 	global distance
+	global state
+	global previousDistance
 	
 	vel_msg = Twist()
 	vel_msg.linear.x = 0
@@ -91,50 +93,47 @@ def computeControlSignal():
 	vel_msg.angular.y = 0
 	vel_msg.angular.z = 0
 	
-	fastVel = .5
-	slowVel = .2
-	largeTurn = .5
-	smallTurn = .1
+	turnSpeed = 0.5
+	velocity = 0.5
 	
-	if distance[0] > 1.5:
-		if distance[1] > 1.5:
-			if distance[2] > 1.5: 			#coast 000
-				vel_msg.linear.x = fastVel
-				print 'coast 000'
-			else:							#slight left 001
-				vel_msg.linear.x = slowVel
-				vel_msg.angular.z = smallTurn
-				print 'slight left 001'
-		else:
-			if distance[2] > 1.5: 			#turn right 010
-				vel_msg.angular.z = -largeTurn
-				print 'turn right 010'
-			else:							#turn left 011
-				vel_msg.angular.z = -largeTurn
-				print 'turn left 011'
+	#coasting
+	if distance[1] > 1.5:
+		if state[0] == 'coast':
+			if state[1] == 'coast':
+				vel_msg.linear.x = velocity
+			elif distance[0] > .75:
+				state[1] = 'coast'
+		elif distance[0] > .75:
+			state[0] = 'coast'
+	#turning
 	else:
-		if distance[1] > 1.5:
-			if distance[2] > 1.5: 			#slight right 100
-				vel_msg.linear.x = slowVel
-				vel_msg.angular.z = -smallTurn
-				print 'slight right 100'
-			else:							#approach slowly 101
-				vel_msg.linear.x = slowVel
-				print 'approach slowly 101'
+		if state[0] == 'blocked':
+			if state[1] == 'blocked':
+				vel_msg.angular.z = -turnSpeed
+			else:
+				state[1] = 'blocked'
 		else:
-			if distance[2] > 1.5: 			#turn right 110
-				vel_msg.angular.z = -largeTurn
-				print 'turn right 110'
-			else:							#turn right 111
-				vel_msg.angular.z = -largeTurn
-				print 'turn right 111'
+			state[0] = 'blocked'
+			
+	#get stuck, no forward progress
+	diffCenter = ((previousDistance[1]-distance[1])**2)
+	diffLeft = ((previousDistance[0]-distance[0])**2)
+	diffRight = ((previousDistance[2]-distance[2])**2)
+	
+	if  (diffCenter > 0 and diffCenter < 0.0025) or (diffLeft > 0 and diffLeft < 0.0025) or (diffRight > 0 and diffRight < 0.0025):
+		state[0] = state[1] = 'blocked'
+		vel_msg.angular.z = -turnSpeed
+	
+	previousDistance[0] = distance[0]
+	previousDistance[1] = distance[1]
+	previousDistance[2] = distance[2]
 
 	return vel_msg
 
 if __name__ == '__main__':
 	rospy.init_node('assign3')
 
-	#initialize laserData somehow
+	#initialize laserData
 	laserData = LaserScan()
 	global currentVel
 	
@@ -146,7 +145,7 @@ if __name__ == '__main__':
 
 	startTime = rospy.get_time()
 	while not rospy.is_shutdown():
-		rospy.sleep(0.4)
+		rospy.sleep(0.3)
 
 		robot_control_signal = computeControlSignal()
 		pubControlSignals.publish(robot_control_signal)
